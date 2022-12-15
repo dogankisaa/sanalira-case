9cd0b6a2adaf445fa941e7041cc83ae1a5ddad04
dogankisaa <kisalarr@outlook.com>
2022-12-15 20:27:05 +0300

    Bank listing and bank screen done

--- lib/core/consts/bank_constants.dart
+++ lib/core/consts/bank_constants.dart
@@ -2,5 +2,37 @@ import 'package:flutter/material.dart';
 
 class BankConstants {
   Color backgroundColor = const Color.fromRGBO(243, 244, 246, 1);
+  Color titleTextColor = const Color.fromRGBO(20, 28, 45, 1);
+  Color subTitleTextColor = const Color.fromRGBO(207, 212, 222, 1);
+  Color chooseBankTextColor = const Color.fromRGBO(207, 212, 222, 1);
+  Color bankLogoBorderColor = const Color.fromRGBO(207, 212, 222, 1);
+  Color bankListNameColor = const Color.fromRGBO(20, 28, 45, 1);
+  Color bankListSubTitleColor = const Color.fromRGBO(207, 212, 222, 1);
+  Color loadingColor = const Color.fromRGBO(102, 204, 120, 1);
+  Color notificationColor = const Color.fromRGBO(102, 204, 120, 1);
+
+  EdgeInsets screenPadding =
+      const EdgeInsets.only(left: 15, right: 15, top: 36);
+
+  BorderRadius currentMoneyRadius = BorderRadius.circular(10);
+  EdgeInsets currentMoneyInsidePadding =
+      const EdgeInsets.symmetric(horizontal: 10);
+  BorderRadius bankListRadius = BorderRadius.circular(10);
+  EdgeInsets bankListInsidePadding = const EdgeInsets.symmetric(horizontal: 10);
+  Radius bankListPaddingRadius = const Radius.circular(10);
+
   String backIcon = "assets/left_arrow.svg";
+  String notificationIcon = "assets/bell.svg";
+  String settingsIcon = "assets/settings.svg";
+  String turkeyIcon = "assets/country_turkey.png";
+  String chooseBankText = "Türk lirası yatırmak için banka seçiniz.";
+  String moneyName = "Türk Lirası";
+  String moneyShortName = "TL";
+  String moneyQuantity = "234";
+  String moneyMark = "₺";
+  String bankListSubtitle = "Havale / EFT ile para gönderin.";
+
+  double currentMoneyHeight = 56;
+  double currentMoneyCountryHeight = 29;
+  double bankListHeight = 72;
 }
--- lib/view/bank_view.dart
+++ lib/view/bank_view.dart
@@ -1,3 +1,4 @@
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
@@ -25,14 +26,229 @@ class BankView extends StatelessWidget {
         _context = context;
         return Scaffold(
             backgroundColor: BankConstants().backgroundColor,
-            appBar: AppBar(
-              leading: SvgPicture.asset(BankConstants().backIcon,
-                  semanticsLabel: 'Acme Logo'),
-              elevation: 0.0,
-              backgroundColor: BankConstants().backgroundColor,
-            ),
-            body: Container());
+            body: Padding(
+                padding: BankConstants().screenPadding,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    header(),
+                    const SizedBox(
+                      height: 21,
+                    ),
+                    currentQuantityCard(context),
+                    const SizedBox(
+                      height: 13,
+                    ),
+                    chooseBankText(context),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    bankList()
+                  ],
+                )));
       },
+    );
+  }
+
+  Row header() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        appBarIcon(BankConstants().backIcon),
+        Row(
+          children: [
+            Stack(
+              children: [
+                appBarIcon(BankConstants().notificationIcon),
+                Positioned(
+                  left: 20,
+                  top: 10,
+                  child: CircleAvatar(
+                    radius: 4,
+                    backgroundColor: BankConstants().notificationColor,
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              width: 9,
+            ),
+            appBarIcon(BankConstants().settingsIcon),
+          ],
+        )
+      ],
+    );
+  }
+
+  Container currentQuantityCard(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: BankConstants().currentMoneyHeight,
+      decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BankConstants().currentMoneyRadius),
+      child: Padding(
+        padding: BankConstants().currentMoneyInsidePadding,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SizedBox(
+                  width: BankConstants().currentMoneyCountryHeight,
+                  height: BankConstants().currentMoneyCountryHeight,
+                  child: Image.asset(RegisterConstants().countryTurkey),
+                ),
+                const SizedBox(
+                  width: 5,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      BankConstants().moneyName,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3!
+                          .copyWith(color: BankConstants().titleTextColor),
+                    ),
+                    Text(
+                      BankConstants().moneyShortName,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline4!
+                          .copyWith(color: BankConstants().subTitleTextColor),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            Text(
+              "${BankConstants().moneyQuantity} ${BankConstants().moneyMark}",
+              style: Theme.of(context)
+                  .textTheme
+                  .headline2!
+                  .copyWith(color: BankConstants().titleTextColor),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Text chooseBankText(BuildContext context) {
+    return Text(
+      BankConstants().chooseBankText,
+      style: Theme.of(context)
+          .textTheme
+          .headline3!
+          .copyWith(color: BankConstants().chooseBankTextColor),
+    );
+  }
+
+  Expanded bankList() {
+    return Expanded(
+      child: FutureBuilder(
+        future: GetBanks().getBanks(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: snapshot.data.length,
+                    itemBuilder: (context, index) {
+                      return bankListCard(
+                          context, snapshot.data[index]["bankName"]);
+                    },
+                  ),
+                ),
+              ],
+            );
+          } else {
+            return Center(
+              child: CircularProgressIndicator(
+                color: BankConstants().loadingColor,
+              ),
+            );
+          }
+        },
+      ),
+    );
+  }
+
+  Column bankListCard(BuildContext context, bankName) {
+    return Column(
+      children: [
+        Container(
+          width: double.infinity,
+          height: BankConstants().bankListHeight,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BankConstants().bankListRadius),
+          child: Padding(
+            padding: BankConstants().bankListInsidePadding,
+            child: Row(
+              children: [
+                SizedBox(
+                  height: 55,
+                  width: 80,
+                  child: DottedBorder(
+                      borderType: BorderType.RRect,
+                      radius: BankConstants().bankListPaddingRadius,
+                      color: BankConstants().bankLogoBorderColor,
+                      strokeWidth: 1,
+                      child: Center(
+                          child: Text(
+                        "Logo",
+                        style: Theme.of(context).textTheme.headline2,
+                      ))),
+                ),
+                const SizedBox(
+                  width: 12,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      bankName,
+                      style: Theme.of(context)
+                          .textTheme
+                          .headline3!
+                          .copyWith(color: BankConstants().bankListNameColor),
+                    ),
+                    Text(
+                      BankConstants().bankListSubtitle,
+                      style: Theme.of(context).textTheme.headline3!.copyWith(
+                          color: BankConstants().bankListSubTitleColor),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 5,
+        )
+      ],
+    );
+  }
+
+  Container appBarIcon(icon) {
+    return Container(
+      height: 36,
+      width: 36,
+      decoration: BoxDecoration(
+          color: Colors.white, borderRadius: BorderRadius.circular(10)),
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: SvgPicture.asset(
+          icon,
+        ),
+      ),
     );
   }
 }
